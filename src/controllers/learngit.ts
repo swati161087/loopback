d01728c4ec14358469f4cb2b1b1afded1f1ec5dd
swati161087 <swati161087@gmail.com>
2021-07-17 10:49:29 +0530

    chnaged

--- src/controllers/learngit.ts
+++ src/controllers/learngit.ts
@@ -1,6 +1,5 @@
 //this is my initial change
 // this is my second change
-// this  i smy third chage
-// this is my fourth comment
 // this is my fifth comment
 // this is my another comment.
+// this is one more comment
